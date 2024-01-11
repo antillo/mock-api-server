a52bd06f81ea62761792557d63be6d53f24f18cd
Antonio Jesus Sanchez Colmenero <asanccol@emeal.nttdata.com>
2024-01-11 19:48:53 +0100

    añadidas mas rutas mock

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
-import { JmbpPostNodes } from "./jbmp_post_nodes";
-
 
 const router = Router()
 
-
-router.use('/jbmp_post_signal', new JmbpPostNodes().router)
+router.post('/jbmp_post_nodes',(req,res)=>{res.status(200).json()})
+router.post('/mminventory_delete_processid',(req,res)=>{res.status(200).json()})
+router.post('/mminventory_service_merge_characteristics',(req,res)=>{res.status(200).json()})
+router.post('/jbpm_get_signal',(req,res)=>{res.status(200).json()})
+router.post('/instances',(req,res)=>{res.status(200).json()})
+router.post('/mminventory_change_product_order',(req,res)=>{res.status(200).json()})
+router.post('/jbmp_post_signal',(req,res)=>{res.status(200).json()})
 
 export default router
