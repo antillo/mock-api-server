af74ee1af2dfcb8ab7f42e815314e9f70603b92e
Antonio Jesus Sanchez Colmenero <asanccol@emeal.nttdata.com>
2024-01-12 07:17:04 +0100

    fix error ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router()
 
-router.post('/jbmp_post_nodes',(req,res)=>{res.status(200).json()})
+router.post('/jbmp_post_nodes/:rfs/:node',(req,res)=>{res.status(200).json()})
 router.post('/mminventory_delete_processid',(req,res)=>{res.status(200).json()})
 router.post('/mminventory_service_merge_characteristics',(req,res)=>{res.status(200).json()})
 router.post('/jbpm_get_signal',(req,res)=>{res.status(200).json()})
